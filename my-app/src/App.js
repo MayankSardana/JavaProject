9d267b638b87fcbddcee15a13a7041b595f96dae
Mayank Sardana <mayanksardana4848@gmail.com>
2024-06-04 11:50:07 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>react app is app app app</h1>
+      <img src = {logo} />
+     </div>
+  );
+}
+
+export default App;
